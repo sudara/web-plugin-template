2b502af6b92d83690b0cd3e87a3f2215bfbcb084
August Pemberton <augustmpemberton@gmail.com>
2023-09-25 23:34:34 +0100

    gitignore works

--- cmake-build-relwithdebinfo/CPM_modules/FindJUCE.cmake
+++
@@ -1,3 +0,0 @@
-include("/Users/imagiro/Documents/audiodev/juce/templates/web-ui/cmake-build-relwithdebinfo/cmake/CPM_0.35.0.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;juce-framework/JUCE;GIT_TAG;7.0.7;EXCLUDE_FROM_ALL;YES")
-set(JUCE_FOUND TRUE)
